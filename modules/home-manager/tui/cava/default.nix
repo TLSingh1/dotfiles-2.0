5b30103d437f20ee55d88a44b98e56f0a15dac19
TLSingh1 <tai8910@gmail.com>
2024-07-13 02:47:37 -0700

    configured cava

--- modules/home-manager/tui/cava/default.nix
+++ modules/home-manager/tui/cava/default.nix
@@ -1,8 +1,18 @@
 {
   programs.cava = {
     enable = true;
-    # settings = {
-    #
-    # };
+    settings = {
+      color = {
+        gradient = 1;
+        gradient_color_1 = "'#aa00ff'";
+        gradient_color_2 = "'#9b04e7'";
+        gradient_color_3 = "'#6c00c8'";
+        gradient_color_4 = "'#5100c4'";
+        gradient_color_5 = "'#3500a1'";
+        gradient_color_6 = "'#3804c0'";
+        gradient_color_7 = "'#038dd0'";
+        gradient_color_8 = "'#00d4ff'";
+      };
+    };
   };
 }
