f588efffe95013b867606b94c9df43e23d86fe1d
TLSingh1 <tai8910@gmail.com>
2024-08-06 02:26:52 -0700

    tweaks

---
+++ modules/home-manager/tui/neovim/lua/plugins/ui/heirline/components/file.lua
@@ -0,0 +1,146 @@
+local conditions = require("heirline.conditions")
+local utils = require("heirline.utils")
+
+local FileNameBlock = {
+  -- let's first set up some attributes needed by this component and its children
+  init = function(self)
+    self.filename = vim.api.nvim_buf_get_name(0)
+  end,
+}
+-- We can now define some children separately and add them later
+
+local FileIcon = {
+  init = function(self)
+    local filename = self.filename
+    local extension = vim.fn.fnamemodify(filename, ":e")
+    self.icon, self.icon_color = require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
+  end,
+  provider = function(self)
+    return self.icon and (self.icon .. " ")
+  end,
+  hl = function(self)
+    return { fg = self.icon_color }
+  end
+}
+
+local FileName = {
+  provider = function(self)
+    -- first, trim the pattern relative to the current directory. For other
+    -- options, see :h filename-modifers
+    local filename = vim.fn.fnamemodify(self.filename, ":.")
+    if filename == "" then return "[No Name]" end
+    -- now, if the filename would occupy more than 1/4th of the available
+    -- space, we trim the file path to its initials
+    -- See Flexible Components section below for dynamic truncation
+    if not conditions.width_percent_below(#filename, 0.25) then
+      filename = vim.fn.pathshorten(filename)
+    end
+    return filename
+  end,
+  hl = { fg = utils.get_highlight("Directory").fg },
+}
+
+local FileFlags = {
+  {
+    condition = function()
+      return vim.bo.modified
+    end,
+    provider = "[+]",
+    hl = { fg = "green" },
+  },
+  {
+    condition = function()
+      return not vim.bo.modifiable or vim.bo.readonly
+    end,
+    provider = "",
+    hl = { fg = "orange" },
+  },
+}
+
+-- Now, let's say that we want the filename color to change if the buffer is
+-- modified. Of course, we could do that directly using the FileName.hl field,
+-- but we'll see how easy it is to alter existing components using a "modifier"
+-- component
+
+local FileNameModifer = {
+  hl = function()
+    if vim.bo.modified then
+      -- use `force` because we need to override the child's hl foreground
+      return { fg = "cyan", bold = true, force = true }
+    end
+  end,
+}
+
+-- let's add the children to our FileNameBlock component
+FileNameBlock = utils.insert(FileNameBlock,
+  FileIcon,
+  utils.insert(FileNameModifer, FileName), -- a new table where FileName is a child of FileNameModifier
+  FileFlags,
+  { provider = '%<' }                      -- this means that the statusline is cut here when there's not enough space
+)
+
+-------------------------------------------------
+
+local FileType = {
+  provider = function()
+    return string.upper(vim.bo.filetype)
+  end,
+  hl = { fg = utils.get_highlight("Type").fg, bold = true },
+}
+
+local FileEncoding = {
+  provider = function()
+    local enc = (vim.bo.fenc ~= '' and vim.bo.fenc) or vim.o.enc -- :h 'enc'
+    return enc ~= 'utf-8' and enc:upper()
+  end
+}
+
+local FileFormat = {
+  provider = function()
+    local fmt = vim.bo.fileformat
+    return fmt ~= 'unix' and fmt:upper()
+  end
+}
+
+local FileSize = {
+  provider = function()
+    -- stackoverflow, compute human readable file size
+    local suffix = { 'b', 'k', 'M', 'G', 'T', 'P', 'E' }
+    local fsize = vim.fn.getfsize(vim.api.nvim_buf_get_name(0))
+    fsize = (fsize < 0 and 0) or fsize
+    if fsize < 1024 then
+      return fsize .. suffix[1]
+    end
+    local i = math.floor((math.log(fsize) / math.log(1024)))
+    return string.format("%.2g%s", fsize / math.pow(1024, i), suffix[i + 1])
+  end
+}
+
+local FileLastModified = {
+  -- did you know? Vim is full of functions!
+  provider = function()
+    local ftime = vim.fn.getftime(vim.api.nvim_buf_get_name(0))
+    return (ftime > 0) and os.date("%c", ftime)
+  end
+}
+
+FileNameBlock = utils.insert(FileNameBlock,
+  FileIcon,
+  utils.insert(FileNameModifer, FileName), -- a new table where FileName is a child of FileNameModifier
+  FileFlags,
+  { provider = '%<' }                      -- this means that the statusline is cut here when there's not enough space
+)
+
+return FileNameBlock
+
+-- return {
+--   FileNameBlock = FileNameBlock,
+--   FileIcon = FileIcon,
+--   FileName = FileName,
+--   FileFlags = FileFlags,
+--   FileType = FileType,
+--   FileEncoding = FileEncoding,
+--   FileFormat = FileFormat,
+--   FileSize = FileSize,
+--   FileLastModified = FileLastModified,
+-- }
--- modules/home-manager/tui/neovim/lua/plugins/ui/heirline/status_line.lua
+++ modules/home-manager/tui/neovim/lua/plugins/ui/heirline/status_line.lua
@@ -1,7 +1,10 @@
 local ViMode = require("plugins.ui.heirline.components.vi-mode")
+local FileNameBlock = require("lua.plugins.ui.heirline.components.file")
+-- local FC = require("plugins.ui.heirline.components.file")
 
 local StatusLine = {
   ViMode,
+  FileNameBlock,
 }
 
 return StatusLine
