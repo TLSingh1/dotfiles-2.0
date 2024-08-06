c4f6491a6c6a6480b0b811cd780eedfc16a02bbc
TLSingh1 <tai8910@gmail.com>
2024-08-05 18:07:56 -0700

    configured cmp

--- modules/home-manager/tui/neovim/lua/plugins/coding/completion/cmp.lua
+++ modules/home-manager/tui/neovim/lua/plugins/coding/completion/cmp.lua
@@ -8,33 +8,32 @@ luasnip.filetype_extend("typescriptreact", {"html"})
 require("luasnip/loaders/from_vscode").load({include = {"html"}})
 require("luasnip/loaders/from_vscode").lazy_load()
 
---   פּ ﯟ   some other good icons
 local kind_icons = {
-	Text = "",
-	Method = "m",
-	Function = "",
-	Constructor = "",
-	Field = "",
-	Variable = "",
-	Class = "",
-	Interface = "",
-	Module = "",
-	Property = "",
-	Unit = "",
-	Value = "",
-	Enum = "",
-	Keyword = "",
-	Snippet = "",
-	Color = "",
-	File = "",
-	Reference = "",
-	Folder = "",
-	EnumMember = "",
-	Constant = "",
-	Struct = "",
-	Event = "",
-	Operator = "",
-	TypeParameter = "",
+  Text = "󰉿 ",
+  Method = "󰆧 ",
+  Function = "󰊕 ",
+  Constructor = " ",
+  Field = "󰜢 ",
+  Variable = "󰀫 ",
+  Class = "󰠱 ",
+  Interface = " ",
+  Module = " ",
+  Property = "󰜢 ",
+  Unit = "󰑭 ",
+  Value = "󰎠 ",
+  Enum = " ",
+  Keyword = "󰌋 ",
+  Snippet = " ",
+  Color = " ",
+  File = "󰈙 ",
+  Reference = "󰈇 ",
+  Folder = "󰉋 ",
+  EnumMember = " ",
+  Constant = "󰏿 ",
+  Struct = "󰙅 ",
+  Event = " ",
+  Operator = "󰆕",
+  TypeParameter = " ",
 }
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
@@ -90,7 +89,7 @@ cmp.setup({
 	formatting = {
 		fields = { "kind", "abbr", "menu" },
 		format = function(entry, vim_item)
-			-- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
 			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
 			vim_item.menu = ({
 				nvim_lsp = "[LSP]",
