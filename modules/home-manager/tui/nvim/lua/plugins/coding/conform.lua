c3d368e9a0f208842b4e3fac2a9a2f6b70093eba
TLSingh1 <tai8910@gmail.com>
2024-08-23 03:56:13 -0700

    tweaks

--- modules/home-manager/tui/nvim/lua/plugins/coding/conform.lua
+++ modules/home-manager/tui/nvim/lua/plugins/coding/conform.lua
@@ -1,13 +1,13 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = "fallback",
-    },
-    formatters_by_ft = {
-      lua = { "stylua" },
-      nix = { "alejandra" },
-    },
-  },
+	"stevearc/conform.nvim",
+	opts = {
+		format_on_save = {
+			timeout_ms = 1000,
+			lsp_format = "fallback",
+		},
+		formatters_by_ft = {
+			lua = { "stylua" },
+			nix = { "alejandra" },
+		},
+	},
 }
--- modules/home-manager/tui/nvim/lua/plugins/coding/typescript-tools.lua
+++ modules/home-manager/tui/nvim/lua/plugins/coding/typescript-tools.lua
@@ -1,5 +1,7 @@
 return {
 	"pmizio/typescript-tools.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-	opts = {},
+	config = function()
+		require("typescript-tools").setup({})
+	end,
 }
