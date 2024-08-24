aa681f59d6d3ea65247610c3ada44983d2510e01
TLSingh1 <tai8910@gmail.com>
2024-08-24 16:31:21 -0700

    twaeaks

--- modules/home-manager/tui/nvim/lua/plugins/ui/telescope.lua
+++ modules/home-manager/tui/nvim/lua/plugins/ui/telescope.lua
@@ -1,7 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	cmd = "Telescope",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
