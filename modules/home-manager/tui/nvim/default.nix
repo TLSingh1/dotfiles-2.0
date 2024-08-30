9581be1d5a13441c327520327fa6b7d881fc69e8
TLSingh1 <tai8910@gmail.com>
2024-08-29 19:07:01 -0700

    tweaks

--- modules/home-manager/tui/nvim/default.nix
+++ modules/home-manager/tui/nvim/default.nix
@@ -25,16 +25,16 @@
       # Coding
       # nvim-treesitter.withAllGrammars
       ultimate-autopair-nvim
-      # fast-cmp
-      # luasnip
-      # friendly-snippets
-      # cmp-nvim-lsp
-      # cmp-buffer
-      # cmp-path
-      # cmp_luasnip
-      # cmp-cmdline
-      # cmp-emoji
-      # nvim-lspconfig
+      fast-cmp
+      luasnip
+      friendly-snippets
+      cmp-nvim-lsp
+      cmp-buffer
+      cmp-path
+      cmp_luasnip
+      cmp-cmdline
+      cmp-emoji
+      nvim-lspconfig
       # none-ls-nvim
       # conform-nvim
       # lsp_lines-nvim
