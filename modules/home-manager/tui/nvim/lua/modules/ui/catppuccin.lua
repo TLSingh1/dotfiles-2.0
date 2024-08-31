6c913eeb33996a56ff1bdaeafe8f91d2195a1c8b
TLSingh1 <tai8910@gmail.com>
2024-08-31 03:11:26 -0700

    custom winbar

--- modules/home-manager/tui/nvim/lua/modules/ui/catppuccin.lua
+++ modules/home-manager/tui/nvim/lua/modules/ui/catppuccin.lua
@@ -2,41 +2,69 @@
 local module_manager = require("core.module_manager")
 
 module_manager.use({
-  name = "catppuccin-nvim",  -- Changed from "catppuccin" to "catppuccin-nvim"
-  config = function()
-    require("catppuccin").setup({
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-      },
-      transparent_background = false,
-      show_end_of_buffer = false,
-      term_colors = true,
-      no_italic = false,
-      no_bold = false,
-      no_underline = false,
-      styles = {
-        comments = { "italic" },
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = { "bold" },
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-      },
-      -- color_overrides = {
-      --   all = {
-      --     base = "#000000",
-      --   },
-      -- },
-    })
+	name = "catppuccin-nvim", -- Changed from "catppuccin" to "catppuccin-nvim"
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = false,
+			show_end_of_buffer = false,
+			term_colors = true,
+			no_italic = false,
+			no_bold = false,
+			no_underline = false,
+			styles = {
+				comments = { "italic" },
+				conditionals = { "italic" },
+				loops = {},
+				functions = {},
+				keywords = { "bold" },
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
+			},
+			-- color_overrides = {
+			--   all = {
+			--     base = "#000000",
+			--   },
+			-- },
+			custom_highlights = function()
+				return {
+					Normal = { bg = "#000000" },
+					-- InactiveWindow = { bg = "#000000" },
+					-- FloatBorder = { fg = "#011826", bg = "#011826" },
+					-- WinSeparator = { fg = "#011826", bg = "#000000" },
+					-- NeoTreeFloatBorder = { fg = "#011826", bg = "#011826" },
+					-- NeoTreeFloatTitle = { fg = "#011826", bg = "#011826" },
+					-- NeoTreeNormal = { bg = "#011826" },
+					-- NeoTreeTabActive = { bg = "#0B2534" },
+					-- NeoTreeTabSeparatorActive = { fg = "#0B2534", bg = "#0B2534" },
+					-- NoiceCmdLine = { bg = "#000000" },
+					-- TelescopePromptNormal = { fg = "#1affff", bg = "#0B2534" },
+					-- TelescopePromptBorder = { fg = "#0B2534", bg = "#0B2534" },
+					-- TelescopePrompt = { fg = "#0B2534", bg = "#0B2534" },
+					-- TelescopeBorder = { fg = "#011826", bg = "#011826" },
+					-- TelescopeSelection = { bg = "#233B48" },
+					-- TelescopeSelectionCaret = { bg = "#233B48" },
+					-- TelescopeTitle = { fg = "#0B2534", bg = "#0B2534" },
+					-- Folded = { bg = "#233B48", style = { "italic", "bold" } },
+					-- CursorLineNr = { style = { "bold" } },
+					-- TreesitterContext = { bg = "#0B2534" },
+					-- TreesitterContextBottom = { bg = "#0B2534" },
+					-- TreesitterContextLineNumber = { bg = "#0B2534" },
+					-- NvimTreeNormalFloat = { bg = "#011826" },
+					-- NvimTreeSignColumn = { bg = "#011826" },
+				}
+			end,
+		})
 
-    vim.cmd.colorscheme("catppuccin")
-  end,
+		vim.cmd.colorscheme("catppuccin")
+	end,
 })
