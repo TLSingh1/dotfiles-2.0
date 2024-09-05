b6b246cc973003730b2f6ddb4f31b61b7b436ce1
TLSingh1 <tai8910@gmail.com>
2024-09-04 17:08:09 -0700

    workspaces for 3 monitors

--- modules/home-manager/wm/hyprland/workspacerules.nix
+++ modules/home-manager/wm/hyprland/workspacerules.nix
@@ -19,10 +19,29 @@
 #     workspace = 10, monitor:HDMI-A-1
 #   '';
 # }
+# {
+#   wayland.windowManager.hyprland.extraConfig = ''
+#     workspace = 1, monitor:HDMI-A-1
+#     workspace = 5, monitor:eDP-1
+#     workspace = 10, monitor:DP-5
+#   '';
+# }
 {
   wayland.windowManager.hyprland.extraConfig = ''
-    workspace = 1, monitor:HDMI-A-1
+    workspace = 1, monitor:eDP-1
+    workspace = 2, monitor:eDP-1
+    workspace = 3, monitor:eDP-1
+    workspace = 4, monitor:eDP-1
     workspace = 5, monitor:eDP-1
+    workspace = 6, monitor:DP-5
+    workspace = 7, monitor:DP-5
+    workspace = 8, monitor:DP-5
+    workspace = 9, monitor:DP-5
     workspace = 10, monitor:DP-5
+    workspace = 11, monitor:HDMI-A-1
+    workspace = 12, monitor:HDMI-A-1
+    workspace = 13, monitor:HDMI-A-1
+    workspace = 14, monitor:HDMI-A-1
+    workspace = 15, monitor:HDMI-A-1
   '';
 }
